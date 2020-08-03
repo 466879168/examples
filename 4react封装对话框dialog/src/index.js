2cad0008f37f6cb785db1ff853e144ecbbc12c29
mengyang <466879168@qq.com>
2020-08-03 18:42:46 +0800

    8.3

--- 4react封装对话框dialog/src/index.js
+++ 4react封装对话框dialog/src/index.js
@@ -7,18 +7,6 @@ let styleObj={
     color:"red",
 }
 
-/*
-1. 基于babel中的语法解析模块（babel-preset-react）把jsx语法编译为react.createElement()结构
-"use strict";
-
-render(React.createElement("h1", {
-    id: 'titleBox',
-    className: 'title',
-    style: styleObj
-}, "\u5B66\u4E60"), root);
-2. 执行React.createElement(type,props,children)会创建一个对象（虚拟dom），对象里面有几个属性 type:h1,props:{id:...,title:...,children...}key:null,ref:null,_proto_:...
-3. reactdom.render(jsx语法最后生成的对象，容器)基于render方法把生成的对象动态创建为dom元素，插入到指定的容器中
-* */
 
 
 
